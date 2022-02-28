520948ce4d22d40de29a130331d9255fb0ce5b96
isakbet <yosephbet@gmail.com>
2022-02-28 12:42:51 +0100

    Bring in result and filter id elements

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// ------ SAVING IN CONSTANTS THE DOM ELEMENTS WE NEED TO WORK WITH ------ //
+
+// this constant allows us to filter the data to be displayed, by dynamically changing its "id" attribute
+const result = document.getElementById("result");
+
+// this constant allows us to access the value of the user's input, i.e. the search word 
+const filter = document.getElementById("filter");
